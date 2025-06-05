0a247da665ce7f553a061c215e558a697f749529
Xander12239 <167882137+Xander12239@users.noreply.github.com>
2025-06-05 15:17:22 -0400

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,64 +1,76 @@
-/*
-	Dopetrope by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+"use strict";
+jQuery( document ).ready( function ( $ ) {
 
-(function($) {
+//for Preloader
 
-	var	$window = $(window),
-		$body = $('body');
+    $( window ).load( function () {
+        $( "#loading" ).fadeOut( 500 );
+    } );
 
-	// Breakpoints.
-		breakpoints({
-			xlarge:  [ '1281px',  '1680px' ],
-			large:   [ '981px',   '1280px' ],
-			medium:  [ '737px',   '980px'  ],
-			small:   [ null,      '736px'  ]
-		});
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+    /*---------------------------------------------*
+     * Mobile menu
+     ---------------------------------------------*/
+    $( '.link' ).find( 'a[href*=#]:not([href=#])' ).click( function () {
+        if ( location.pathname.replace( /^\//, '' ) == this.pathname.replace( /^\//, '' ) && location.hostname == this.hostname ) {
+            var target = $( this.hash );
+            target = target.length ? target : $( '[name=' + this.hash.slice( 1 ) + ']' );
+            if ( target.length ) {
+                $( 'html,body' ).animate( {
+                    scrollTop: ( target.offset().top - 80 )
+                }, 1000 );
+                if ( $( '.navbar-toggle' ).css( 'display' ) != 'none' ) {
+                    $( this ).parents( '.container' ).find( ".navbar-toggle" ).trigger( "click" );
+                }
+                return false;
+            }
+        }
+    } );
 
-	// Dropdowns.
-		$('#nav > ul').dropotron({
-			mode: 'fade',
-			noOpenerFade: true,
-			alignment: 'center'
-		});
 
-	// Nav.
 
-		// Title Bar.
-			$(
-				'<div id="titleBar">' +
-					'<a href="#navPanel" class="toggle"></a>' +
-				'</div>'
-			)
-				.appendTo($body);
+    /*---------------------------------------------*
+     * WOW
+     ---------------------------------------------*/
 
-		// Panel.
-			$(
-				'<div id="navPanel">' +
-					'<nav>' +
-						$('#nav').navList() +
-					'</nav>' +
-				'</div>'
-			)
-				.appendTo($body)
-				.panel({
-					delay: 500,
-					hideOnClick: true,
-					hideOnSwipe: true,
-					resetScroll: true,
-					resetForms: true,
-					side: 'left',
-					target: $body,
-					visibleClass: 'navPanel-visible'
-				});
+    var wow = new WOW( {
+        mobile: false // trigger animations on mobile devices (default is true)
+    } );
+    wow.init();
 
-})(jQuery);
+// magnificPopup
+
+    $( '.popup-img' ).magnificPopup( {
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    } );
+
+    $( '.video-link' ).magnificPopup( {
+        type: 'iframe'
+    } );
+
+
+    $( '.mouse-scroll' ).bind( 'click', function () {
+        $( 'html , body' ).stop().animate( {
+            scrollTop: $( $( this ).attr( 'href' ) ).offset().top - 60
+        }, 1500, 'easeInOutExpo' );
+        event.preventDefault();
+    } );
+
+
+} );
+
+
+
+
+
+
+
+
+
+
+
+
+
